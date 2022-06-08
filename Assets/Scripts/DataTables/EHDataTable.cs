b37f219f312cf24227d9d514ed51b4e7f28e1264
Ryan Andersen <ryan.andersen05@gmail.com>
2022-06-08 00:41:37 -0400

    Adding base data table logic

---
+++ Assets/Scripts/DataTables/EHDataTable.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EHTableRow
+{
+    public string RowId;
+}
+
+public class EHDataTable<T> : ScriptableObject where T : EHTableRow
+{
+    [SerializeField]
+    private T[] DataTableRows;
+
+    private Dictionary<string, T> TableRowsMap = new Dictionary<string, T>();
+
+    public virtual void InitializeDataTable()
+    {
+        TableRowsMap.Clear();
+        foreach (T Row in DataTableRows)
+        {
+            TableRowsMap.Add(Row.RowId, Row);
+        }
+    }
+
+    public T FindRow(string RowId, bool ShouldLogMissing = true)
+    {
+        if (TableRowsMap.ContainsKey(RowId))
+        {
+            return TableRowsMap[RowId];
+        }
+
+        if (ShouldLogMissing)
+        {
+            Debug.LogWarning("Row Was Not Found In DataTable: " + RowId);
+        }
+        return null;
+    }
+}
--- Assets/Scripts/GameManagers/EHPlayerState.cs
+++ Assets/Scripts/GameManagers/EHPlayerState.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class EHPlayerState : MonoBehaviour
 {
-    public EHCharacter AssociatedPlayerCharacter;
-    public EHPlayerController AssociatedPlayerController;
+    public EHCharacter AssociatedPlayerCharacter { get; private set; }
+    public EHPlayerController AssociatedPlayerController { get; private set; }
+
+    public void PossessPlayerCharacter(EHCharacter PlayerCharacter)
+    {
+        this.AssociatedPlayerCharacter = PlayerCharacter;
+    }
     
-    
+    public void PossessPlayerController(EHPlayerController PlayerController)
+    {
+        AssociatedPlayerController = PlayerController;
+        if (!AssociatedPlayerController) return;
+        
+        if (AssociatedPlayerCharacter)
+        {
+            AssociatedPlayerCharacter.SetUpControllerInput(AssociatedPlayerController);
+        }
+    }
 }
