172d5bc34715a0b6e12c6dca5eb88fbf88e6669d
Ryan Andersen <ryan.andersen05@gmail.com>
2022-08-21 16:51:18 -0400

    BEEG Update

--- Assets/Scripts/UI/EHBaseUIScene.cs
+++ Assets/Scripts/UI/EHBaseUIScene.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EHBaseUIScene : MonoBehaviour
 {
     public bool IsShown { get; private set; }
-    
+
     public virtual void OnShow()
     {
         IsShown = true;
@@ -15,4 +16,38 @@ public class EHBaseUIScene : MonoBehaviour
     {
         IsShown = false;
     }
+
+    public void PushMe()
+    {
+        
+    }
+
+    public void PopMe()
+    {
+        EHBaseGameHUD GameHUD = GetGameHUD<EHBaseGameHUD>();
+        if (GameHUD)
+        {
+            GameHUD.PopScene(this);
+        }
+    }
+
+    protected T GetGameHUD<T>() where T : EHBaseGameHUD
+    {
+        if (EHGameInstance.Instance)
+        {
+            return (T)EHGameInstance.Instance.GameHUD;
+        }
+
+        return null;
+    }
+    
+    #region selectable UI
+
+    private List<EHSelectableUI> AllSelectableUI;
+    private void InitializeSelectableUI()
+    {
+        EHSelectableUI[] AllSelectableUIComponents = GetComponentsInChildren<EHSelectableUI>();
+        AllSelectableUI = AllSelectableUIComponents.ToList();
+    }
+    #endregion selectable UI
 }
